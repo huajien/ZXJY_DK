43088cd90ee4b08173f26700095efde01228910f
HUAJIEN <3514825663@qq.com>
2023-11-10 17:47:25 +0800

    加载用户

---
+++ loadUsers.py
@@ -0,0 +1,18 @@
+import json
+import os
+
+
+def loadUserFiles():
+    allUsers = ""
+    path = os.getcwd() + os.sep + "userData.json"
+    if os.path.exists(path):
+        print("存在userData.json文件")
+        with open(path, encoding="utf-8") as f:
+            lines = f.readlines()
+            for line in lines:
+                allUsers = allUsers + line + '\n'
+        return json.loads(allUsers)
+    else:
+        localPath = os.listdir(os.getcwd())
+        print(f"\033[31m无法找到 {path} 文件，\n请先执行addUser.py：\n{localPath}\033[0m")
+        return False
