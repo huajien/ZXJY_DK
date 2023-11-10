6b050bae7d64977a6001975a85ca8130f0c9c88d
HUAJIEN <3514825663@qq.com>
2023-11-10 17:47:57 +0800

    用户配置

---
+++ parsUserConfig.py
@@ -0,0 +1,34 @@
+import version
+
+import Login
+import apitoken
+import punchCard
+
+
+def LoadUserConfig(user):
+    if not user["enable"]:
+        print(user['remark'], '未启用打卡，即将跳过')
+        return False
+    else:
+        print('用户' + user['remark'], '已启用，即将打卡')
+        state, token = apitoken.get_apitoken()
+
+        if not state:
+            print('用户', user['remark'], '获取Token失败', token)
+        else:
+            # print(version.AppVersion(user["pushKey"]))
+            if (version.AppVersion(user["pushKey"]) == 1):
+                state, returnData = (Login.login(user, token))
+                if state:
+                    uid = returnData["data"]["uid"]
+                    token = returnData["data"]["UserToken"]
+                    # print(uid)
+                    state, returnData = punchCard.clockIn(user, uid, token)
+                    if not state:
+                        print(returnData)
+                    else:
+                        print(returnData)
+                else:
+                    print(returnData)
+            else:
+                print('app版本有更新')
