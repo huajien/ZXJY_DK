fdae2fdbe7b2a69f2f16616b4c0efa95228bef88
HUAJIEN <3514825663@qq.com>
2023-11-12 13:58:08 +0800

    gpt报告

--- parsUserConfig.py
+++ parsUserConfig.py
@@ -1,9 +1,17 @@
 import version
 import pushMessage
+import sha256Encode
 import Login
 import apitoken
+import json
+import random
 import punchCard
+import gptReport
 import time
+import datetime
+import main
+import requests
+
 
 def LoadUserConfig(user):
     if not user["enabled"]:
@@ -23,6 +31,7 @@ def LoadUserConfig(user):
                     uid = loginReturnData["data"]["uid"]
                     token = loginReturnData["data"]["UserToken"]
                     # print(uid)
+                    '''
                     state, returnData = punchCard.clockIn(user, uid, token)
                     if not state:
                         print(f"{returnData}{time.strftime('%Y')}年{time.strftime('%m')}月{time.strftime('%d')}日 打卡失败" )
@@ -34,6 +43,204 @@ def LoadUserConfig(user):
                         pushMessage.pushMessage('职校家园打卡成功提醒',
                                                 f"职校家园 {user['remark']} {time.strftime('%Y')}年{time.strftime('%m')}月{time.strftime('%d')}日 {user['remark']} ",
                                                 user["pushKey"])
+                        '''
+                    if user["report"]:
+                        print(f"用户{user['remark']} 开启日报")
+                        currentDate = datetime.date.today()
+                        isLastDayOfMonth = (currentDate + datetime.timedelta(days=1)).month != currentDate.month
+
+                        jobTitleState, jobTitleReturnData = gptReport.getJobTitle(user, uid, token)
+                        if jobTitleState:
+#----------------------------日报
+                            retry_count = 0
+                            while retry_count < 5:
+                                try:
+                                    dailyRreportContent = gptReport.gptConfig(
+                                        f"写一个{time.strftime('%Y')}年{time.strftime('%m')}月{time.strftime('%d')}日"
+                                        f"{jobTitleReturnData}的工作日报只要一小段60字左右返回一段json数据数据中有 实习项目，实习记录，实习总结。三个不要其他的东西")
+                                    dailyDataRreportContent = json.loads(dailyRreportContent)
+                                    if dailyDataRreportContent['实习项目'] and dailyDataRreportContent['实习记录'] and \
+                                            dailyDataRreportContent['实习总结']:
+                                        print(f"日报信息gpt已完成准备填写日报")
+                                        data = {
+                                            "uid": uid,
+                                            "starttime": time.strftime("%Y-%m-%d"),
+                                            "project": dailyDataRreportContent['实习项目'],
+                                            "address": user['address'],
+                                            "record": dailyDataRreportContent['实习记录'],
+                                            "summary": dailyDataRreportContent['实习总结'],
+                                            "dtype": "1",
+                                        }
+                                        # Sign = sha256Encode.encodeSha256('Anything_2023', json.dumps(data, separators=(',', ':')) + token)
+                                        Sign = sha256Encode.encodeSha256('Anything_2023', json.dumps(data) + token)
+                                        headers = {
+                                            "os": "android",
+                                            "phone": user["deviceModel"],
+                                            "appversion": "59",
+                                            "sign": Sign,
+                                            "timestamp": str(int(time.time() * 1000)),
+                                            "token": token,
+                                            "cl_ip": f"192.168.31.{random.randint(10, 200)}",
+                                            "content-type": "application/json;charset=utf-8",
+                                            "Content-Length": str(len(str(data))),
+                                            "accept-encoding": "gzip, deflate",
+                                            "user-agent": "okhttp/3.14.9"
+                                        }
+
+                                        url = "https://sxbaapp.zcj.jyt.henan.gov.cn/api/ReportHandler.ashx"
+                                        try:
+                                            response = requests.post(url, headers=headers, data=json.dumps(data))
+                                            response.raise_for_status()
+                                            data = response.json()
+                                            if (data["code"] == 1001):
+                                                print(f'{user["remark"]} 日报填写完成')
+                                                return True
+                                            else:
+                                                print(f'日报状态失败{data["code"]} 错误代码{data["msg"]}')
+                                                pushMessage.pushMessage('职校家园日报失败',
+                                                                        f'日报账户 {user["remark"]} 日报状态代码{data["code"]} 错误信息{data["msg"]}',
+                                                                        user["pushKey"])
+                                                return False, f'日报账户 {user["remark"]} 日报状态代码{data["code"]} 错误信息{data["msg"]}'
+                                        except requests.exceptions.RequestException as error:
+                                            pushMessage.pushMessage('职校家园日报失败',
+                                                                    f"发生请求异常：{error} 请求日报异常",
+                                                                    user["pushKey"])
+                                            return False, f"发生请求异常：{error} 请求日报异常"
+                                        break
+                                    else:
+                                        retry_count += 1
+                                except Exception as error:
+                                    print(f"未找到 Error: {error}")
+                                    retry_count += 1
+
+#----------------------------周报
+                            if datetime.datetime.now().weekday() == 6:
+                                retry_count = 0
+                                while retry_count < 5:
+                                    try:
+                                        weeklyRreportContent = gptReport.gptConfig(
+                                            f"写一个{time.strftime('%Y')}年{time.strftime('%m')}月{time.strftime('%d')}每周"
+                                            f"{jobTitleReturnData}的工作周报只要一小段60字左右返回一段json数据数据中有 实习项目，实习记录，实习总结。三个不要其他的东西")
+                                        weekDataRreportContent = json.loads(weeklyRreportContent)
+                                        if weekDataRreportContent['实习项目'] and weekDataRreportContent['实习记录'] and \
+                                                weekDataRreportContent['实习总结']:
+                                            print(f"周报信息gpt已完成")
+                                            data = {
+                                                "uid": uid,
+                                                "starttime": (datetime.date.today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d'),
+                                                "project": dailyDataRreportContent['实习项目'],
+                                                "address": user['address'],
+                                                "record": dailyDataRreportContent['实习记录'],
+                                                "summary": dailyDataRreportContent['实习总结'],
+                                                "dtype": "2",
+                                                "stype": "2",
+                                                "endtime": time.strftime("%Y-%m-%d")
+
+                                            }
+                                            # Sign = sha256Encode.encodeSha256('Anything_2023', json.dumps(data, separators=(',', ':')) + token)
+                                            Sign = sha256Encode.encodeSha256('Anything_2023', json.dumps(data) + token)
+                                            headers = {
+                                                "os": "android",
+                                                "phone": user["deviceModel"],
+                                                "appversion": "59",
+                                                "sign": Sign,
+                                                "timestamp": str(int(time.time() * 1000)),
+                                                "token": token,
+                                                "cl_ip": f"192.168.31.{random.randint(10, 200)}",
+                                                "content-type": "application/json;charset=utf-8",
+                                                "Content-Length": str(len(str(data))),
+                                                "accept-encoding": "gzip, deflate",
+                                                "user-agent": "okhttp/3.14.9"
+                                            }
+
+                                            try:
+                                                response = requests.post(url, headers=headers, data=json.dumps(data))
+                                                response.raise_for_status()
+                                                data = response.json()
+                                                if (data["code"] == 1001):
+                                                    print(f'{user["remark"]} 周报填写完成')
+                                                    return True
+                                                else:
+                                                    print(f'周报状态失败{data["code"]} 错误代码{data["msg"]}')
+                                                    pushMessage.pushMessage('职校家园周报失败',
+                                                                            f'周报账户 {user["remark"]} 周报状态代码{data["code"]} 错误信息{data["msg"]}',
+                                                                            user["pushKey"])
+                                                    return False, f'周报账户 {user["remark"]} 周报状态代码{data["code"]} 错误信息{data["msg"]}'
+                                            except requests.exceptions.RequestException as error:
+                                                pushMessage.pushMessage('职校家园周报失败',
+                                                                        f"发生请求异常：{error} 请求周报异常",
+                                                                        user["pushKey"])
+                                                return False, f"发生请求异常：{error} 请求周报异常"
+                                            break
+                                        else:
+                                            retry_count += 1
+                                    except Exception as error:
+                                        print(f"未找到 Error: {error}")
+                                        retry_count += 1
+#---------------------------- 月报
+                            if isLastDayOfMonth:
+                                retry_count = 0
+                                while retry_count < 5:
+                                    try:
+                                        monthlyRreportContent = gptReport.gptConfig(
+                                            f"写一个{time.strftime('%Y')}年{time.strftime('%m')}月{time.strftime('%d')}每周"
+                                            f"{jobTitleReturnData}的工作月报告只要一小段60字左右返回一段json数据数据中有 实习项目，实习记录，实习总结。三个不要其他的东西")
+                                        monthlyDataRreportContent = json.loads(monthlyRreportContent)
+                                        if monthlyDataRreportContent['实习项目'] and monthlyDataRreportContent[
+                                            '实习记录'] and monthlyDataRreportContent['实习总结']:
+                                            print(f"月报信息gpt已完成")
+                                            data = {
+                                                "uid": uid,
+                                                "starttime": (datetime.date.today() - datetime.timedelta(days=31)).strftime('%Y-%m-%d'),
+                                                "project": dailyDataRreportContent['实习项目'],
+                                                "address": user['address'],
+                                                "record": dailyDataRreportContent['实习记录'],
+                                                "summary": dailyDataRreportContent['实习总结'],
+                                                "dtype": "2",
+                                                "stype": "3",
+                                                "endtime": time.strftime("%Y-%m-%d")
+
+                                            }
+                                            # Sign = sha256Encode.encodeSha256('Anything_2023', json.dumps(data, separators=(',', ':')) + token)
+                                            Sign = sha256Encode.encodeSha256('Anything_2023', json.dumps(data) + token)
+                                            headers = {
+                                                "os": "android",
+                                                "phone": user["deviceModel"],
+                                                "appversion": "59",
+                                                "sign": Sign,
+                                                "timestamp": str(int(time.time() * 1000)),
+                                                "token": token,
+                                                "cl_ip": f"192.168.31.{random.randint(10, 200)}",
+                                                "content-type": "application/json;charset=utf-8",
+                                                "Content-Length": str(len(str(data))),
+                                                "accept-encoding": "gzip, deflate",
+                                                "user-agent": "okhttp/3.14.9"
+                                            }
+
+                                            try:
+                                                response = requests.post(url, headers=headers, data=json.dumps(data))
+                                                response.raise_for_status()
+                                                data = response.json()
+                                                if (data["code"] == 1001):
+                                                    print(f'{user["remark"]} 月报填写完成')
+                                                    return True
+                                                else:
+                                                    print(f'月报状态失败{data["code"]} 错误代码{data["msg"]}')
+                                                    pushMessage.pushMessage('职校家园月报失败',
+                                                                            f'月报账户 {user["remark"]} 月报状态代码{data["code"]} 错误信息{data["msg"]}',
+                                                                            user["pushKey"])
+                                                    return False, f'月报账户 {user["remark"]} 月报状态代码{data["code"]} 错误信息{data["msg"]}'
+                                            except requests.exceptions.RequestException as error:
+                                                pushMessage.pushMessage('职校家园月失败',
+                                                                        f"发生请求异常：{error} 请求月报异常",
+                                                                        user["pushKey"])
+                                                return False, f"发生请求异常：{error} 请求月报异常"
+                                            break
+                                        else:
+                                            retry_count += 1
+                                    except Exception as error:
+                                        print(f"未找到 Error: {error}")
+                                        retry_count += 1
                 else:
                     print(f'职校家园 {user["remark"]} {loginReturnData}')
                     pushMessage.pushMessage('职校家园登录失败提醒',
@@ -41,3 +248,6 @@ def LoadUserConfig(user):
                                             user["pushKey"])
             else:
                 print('app版本有变化停止自动打卡请前往\n https://github.com/huajien/ZXJY_DK \n 更新新版本')
+
+if __name__ == '__main__':
+    main.main()
