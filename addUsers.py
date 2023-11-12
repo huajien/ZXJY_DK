26b5f25d7f4e407a7ba92815213376aadef58213
HUAJIEN <3514825663@qq.com>
2023-11-12 13:58:31 +0800

    添加ai chatgpt日报功能

--- addUsers.py
+++ addUsers.py
@@ -36,7 +36,7 @@ def obtainCoordinates(address):
         1)
 
 
-def checkUserData(filename, enabled, remark, phone, password, deviceModel, address, PushPlus_token):
+def checkUserData(filename, enabled, remark, phone, password, deviceModel, address, PushPlus_token, report):
     deviceId = ''.join(random.choice(string.digits + 'abcdef') for _ in range(36))
     longitude, latitude = obtainCoordinates(address)
     newdata = [
@@ -51,9 +51,14 @@ def checkUserData(filename, enabled, remark, phone, password, deviceModel, addre
             "longitude": longitude,
             "latitude": latitude,
             "pushKey": PushPlus_token,
+            "report": report
         }
     ]
     write_user(filename=filename, newdata=newdata)
+
+
+# 职家家园添加用户
+# 来源 https://github.com/zycn0910/Sign-ZXJY
 
 
 if __name__ == '__main__':
@@ -62,9 +67,9 @@ if __name__ == '__main__':
         with open(filename, 'w') as f:
             json.dump([], f)
     enabled = input("是否开启打卡y or n（默认y）：")
-    if enabled == "y" or "Y":
+    if enabled == "y" or enabled == "Y":
         enabled = True
-    elif enabled == "n" or "N":
+    elif enabled == "n" or enabled == "N":
         enabled = False
     else:
         enabled = True
@@ -92,10 +97,51 @@ if __name__ == '__main__':
     address = input("输入打卡地址例如 河南省郑州市郑东新区正光路11号:\n")
     # 推送push
     PushPlus_token = input("PushPlus_token: ")
-    userdata = checkUserData(filename, enabled, name, phone, password,
-                             deviceModel, address,
-                             PushPlus_token)
-    print(
-        f"添加时间 {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())} 用户信息姓名{name} 手机号{phone} 密码{password} 手机型号{deviceModel} 打卡位置 {address}")
-    pushMessage.pushMessage(f"添加{name}用户成功",
-                            f"开关{enabled} 别名{name} 手机号 {phone} 密码{password} 设备型号{deviceModel} 打卡位置{address}",PushPlus_token)
+    # 报告开关
+    report = input("是否开启日报周报月报 y or n（默认y）：")
+    if report == "y" or report == "Y":
+        report = True
+    elif report == "n" or report == "N":
+        report = False
+    else:
+        report = True
+    path = os.getcwd() + os.sep + "aiReport.json"
+    if os.path.exists(path):
+        with open(path, 'r') as f:
+            data = json.load(f)
+        for entry in data:
+            if "api_key" in entry:
+                reportSwitch = 0
+                break
+            else:
+                reportSwitch = 1
+    else:
+        reportSwitch = 1
+
+    if reportSwitch:
+        pattern = re.compile(r'^sk-[a-zA-Z0-9]{48}$')
+        while True:
+            print("获取 key https://api.chatanywhere.org/v1/oauth/free/github/render ")
+            api_key = input("输入你的key：")
+            if pattern.search(api_key):
+                newdata = [{
+                    "api_key": api_key
+                }]
+                with open(path, 'w') as f:
+                    json.dump(newdata, f, indent=2)
+                break
+            else:
+                print("key格式错误，请重新输入。\n")
+
+        userdata = checkUserData(filename, enabled, name, phone, password,
+                                 deviceModel, address,
+                                 PushPlus_token, report)
+        print(
+            f"添加时间 {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())} 用户信息姓名{name} 手机号{phone} 密码{password} 手机型号{deviceModel} 打卡位置 {address}")
+        pushMessage.pushMessage(f"添加{name}用户成功",
+                                f"开关{enabled} 别名{name} 手机号 {phone} 密码{password} 设备型号{deviceModel} 打卡位置{address}",
+                                PushPlus_token)
+    else:
+        userdata = checkUserData(filename, enabled, name, phone, password,
+                                 deviceModel, address,
+                                 PushPlus_token, report)
