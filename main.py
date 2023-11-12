9f02707e8516047206af09479b027d36b97ebc0a
HUAJIEN <3514825663@qq.com>
2023-11-12 13:58:58 +0800

    方便调用

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import loadUsers
 import parsUserConfig
 import pushMessage
 
-if __name__ == '__main__':
+def main():
     AllUsers = loadUsers.loadUserFiles()
     for user in AllUsers:
         try:
             delay = int(random.uniform(30, 120))
+            # delay = int(1)
             print(f'{user["remark"]} 延时 ' + str(delay) + ' 秒')
             time.sleep(delay)
-            print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
             print(f'{user["remark"]} 打卡时间 ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
             parsUserConfig.LoadUserConfig(user)
         except Exception as error:
@@ -20,3 +20,5 @@ if __name__ == '__main__':
                                     '用户' + user["remark"] + user[
                                         "phone"] + '职校家园打卡失败,' + '具体错误信息：' + str(
                                         error), user["pushKey"])
+if __name__ == '__main__':
+    main()
