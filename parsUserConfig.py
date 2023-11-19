73d99ed46eb4f8ce064d10e80ef52f69e8bde486
HUAJIEN <3514825663@qq.com>
2023-11-19 15:50:21 +0800

    添加用户单独填写日报周报月报可以不开打卡

--- parsUserConfig.py
+++ parsUserConfig.py
@@ -10,7 +10,26 @@ import sendReport
 def LoadUserConfig(user):
     if not user["enabled"]:
         print(user['remark'], '未启用打卡，即将跳过')
-        return False
+    else:
+        print('用户' + user['remark'], '已启用，即将打卡')
+        getApiState, getApiToken = apitoken.get_apitoken()
+
+        if not getApiState:
+            print('用户', user['remark'], '获取Token失败', getApiToken)
+        else:
+            # print(version.AppVersion(user["pushKey"]))
+            if (version.AppVersion(user["pushKey"]) == 1):
+                loginState, loginReturnData = Login.login(user, getApiToken)
+                if loginState:
+                    uid = loginReturnData["data"]["uid"]
+                    token = loginReturnData["data"]["UserToken"]
+                    punchCard.clockIn(user, uid, token)
+            else:
+                print('app版本有变化停止自动打卡请前往\n https://github.com/huajien/ZXJY_DK \n 更新新版本')
+
+
+    if not user["report"]:
+        print(user['report'], '未启用日报周报月报，即将跳过')
     else:
         print('用户' + user['remark'], '已启用，即将打卡')
         getApiState, getApiToken = apitoken.get_apitoken()
@@ -24,31 +43,9 @@ def LoadUserConfig(user):
                 if loginState:
                     uid = loginReturnData["data"]["uid"]
                     token = loginReturnData["data"]["UserToken"]
-                    # print(uid)
-
-                    state, returnData = punchCard.clockIn(user, uid, token)
-                    if not state:
-                        print(
-                            f"{returnData}{time.strftime('%Y')}年{time.strftime('%m')}月{time.strftime('%d')}日 打卡失败")
-                        pushMessage.pushMessage('职校家园打卡失败提醒',
-                                                f"{returnData}{time.strftime('%Y')}年{time.strftime('%m')}月{time.strftime('%d')}日 打卡失败",
-                                                user["pushKey"])
-                    else:
-                        print(f"{returnData}打卡成功")
-                        pushMessage.pushMessage('职校家园打卡成功提醒',
-                                                f"职校家园 {user['remark']} {time.strftime('%Y')}年{time.strftime('%m')}月{time.strftime('%d')}日 {user['remark']} ",
-                                                user["pushKey"])
-
-                    if user["report"]:
-                        sendReport.report(user,uid,token)
-                else:
-                    print(f'职校家园 {user["remark"]} {loginReturnData}')
-                    pushMessage.pushMessage('职校家园登录失败提醒',
-                                            f'职校家园 {user["remark"]} {loginReturnData}',
-                                            user["pushKey"])
+                    sendReport.report(user, uid, token)
             else:
                 print('app版本有变化停止自动打卡请前往\n https://github.com/huajien/ZXJY_DK \n 更新新版本')
-
 
 if __name__ == '__main__':
     main.main()
