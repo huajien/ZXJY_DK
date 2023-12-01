8f5a65ea5a381feac169f3104e86df2d4c87bb99
HUAJIEN <3514825663@qq.com>
2023-12-01 16:34:22 +0800

    去除无用的库和代码

--- main.py
+++ main.py
@@ -1,28 +1,12 @@
-import random
-import time
 import loadUsers
 import parsUserConfig
-import pushMessage
 from inputimeout import inputimeout, TimeoutOccurred
 import addUsers
 
 
 def winmain(AllUsers):
     for user in AllUsers:
-        try:
-            delay = int(random.uniform(30, 120))
-            # delay = int(1)
-            print(f'{user["remark"]} 延时 ' + str(delay) + ' 秒')
-            time.sleep(delay)
-            print(f'{user["remark"]} 打卡时间 ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
             parsUserConfig.LoadUserConfig(user)
-        except Exception as error:
-            print('打卡失败' + str(error))
-            pushMessage.pushMessage('职校家园打卡失败',
-                                    '用户' + user["remark"] + user[
-                                        "phone"] + '职校家园打卡失败,' + '具体错误信息：' + str(
-                                        error), user["pushKey"])
-
 
 
 def main():
