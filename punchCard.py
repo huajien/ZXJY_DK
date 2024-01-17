69661c1bce13f2093a1de390d615ebab0f7505ac
HUAJIEN <3514825663@qq.com>
2024-01-17 22:38:19 +0800

    修复打卡使用参数错误 phonetype

--- punchCard.py
+++ punchCard.py
@@ -4,7 +4,7 @@ import time
 import requests
 import pushMessage
 import sha256Encode
-
+import main
 
 def clockIn(user, uid, token):
     longitude = round(float(user["longitude"]) + random.uniform(-0.00005, 0.00005), 6)
@@ -13,7 +13,7 @@ def clockIn(user, uid, token):
         "dtype": 1,
         "uid": uid,
         "address": user["address"],
-        "phonetype": user["phone"],
+        "phonetype": user["deviceModel"],
         "probability": 0,
         "longitude": longitude,
         "latitude": latitude
@@ -28,7 +28,7 @@ def clockIn(user, uid, token):
         "timestamp": str(int(time.time() * 1000)),
         "token": token,
         "cl_ip": f"192.168.31.{random.randint(10, 200)}",
-        "content-type": "application/json;charset=utf-8",
+        "content-type": "application/json;charset=UTF-8",
         "Content-Length": str(len(str(data))),
         "accept-encoding": "gzip, deflate",
         "user-agent": "okhttp/3.14.9"
@@ -58,3 +58,5 @@ def clockIn(user, uid, token):
     except requests.exceptions.RequestException as error:
         pushMessage.pushMessage('职校家园打卡失败', f"发生请求异常：{error} 请求打卡异常", user["pushKey"])
         return False, f"发生请求异常：{error} 请求登录异常"
+if __name__ == '__main__':
+    main.main()
