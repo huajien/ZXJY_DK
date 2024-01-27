345e19c4823e3e13ad3b4e0a603498c4ef019977
HUAJIEN <3514825663@qq.com>
2024-01-27 20:55:08 +0800

    inputimeout改为30秒

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ def main():
     if not AllUsers:
         addUsers.main()
     try:
-        print('不输入默认60秒自动进行打卡当前用户列表')
+        print('不输入默认30秒自动进行打卡当前用户列表')
         print('是否添加新用户 y or n（默认n执行单次打卡）：')
-        addUserChoice = inputimeout(timeout=60).lower()
+        addUserChoice = inputimeout(timeout=30).lower()
     except TimeoutOccurred:
         addUserChoice = "n"
     if addUserChoice == "y":
