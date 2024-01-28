9a7c8e218bc4bb67105af0877c33c8d1a5c99d2c
HUAJIEN <3514825663@qq.com>
2024-01-28 19:59:50 +0800

    优化版本控制

--- version.py
+++ version.py
@@ -4,7 +4,7 @@ import pushMessage
 
 def AppVersion(pushKey):
     AdaptedVersion = 'v1.4.4'
-    ZxjyVersion = 0.193
+    ZxjyVersion = 0.493
     header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'}
     url = "https://www.yuque.com/huaji-kiyl5/kb/br0dfuykwe7ryku3"
     response = requests.get(url, headers=header).content
