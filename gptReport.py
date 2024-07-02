44e56560eef82d32bfde04bc14360137cdbeea07
HUAJIEN <3514825663@qq.com>
2024-07-02 08:47:38 +0800

    优化prompt词

--- gptReport.py
+++ gptReport.py
@@ -1,5 +1,4 @@
-
-#来源openai接口来源 https://github.com/chatanywhere/GPT_API_free/blob/main/demo.py
+# 来源openai接口来源 https://github.com/chatanywhere/GPT_API_free/blob/main/demo.py
 import openai
 import os
 import json
@@ -9,6 +8,7 @@ import requests
 import pushMessage
 import sha256Encode
 import main
+
 
 def gpt_35_api_stream(messages: list):
     try:
@@ -22,7 +22,8 @@ def gpt_35_api_stream(messages: list):
     except Exception as err:
         return {'error': f'OpenAI API 异常: {err}'}
 
-def getJobTitle(user,uid,token):
+
+def getJobTitle(user, uid, token):
     data = {
         "dtype": "1",
         "uid": uid
@@ -52,16 +53,15 @@ def getJobTitle(user,uid,token):
             workingState = data['data']['bmlist'][0]['bmstatus']
             if workingState == "正在实习":
                 print(f"当前状态是{workingState}")
-                return True,jobTitle
+                return True, jobTitle
             else:
                 print(f"你没有实习中")
-                pushMessage.pushMessage("职校家园提醒",f"你没有实习或者未开始",user["pushKey"])
-                return False,f"你没有在实习中"
+                pushMessage.pushMessage("职校家园提醒", f"你没有实习或者未开始", user["pushKey"])
+                return False, f"你没有在实习中"
         except KeyError as error:
             print(f"找不到{error}键对应数据")
     except requests.exceptions.RequestException as error:
         return f"找不到{error}键对应数据"
-
 
 
 def loadUserFiles():
@@ -74,15 +74,28 @@ def loadUserFiles():
         print(f"\033[93m无法找到 {path} 文件，\n请先执行addUser.py：\n{localPath}\033[0m")
         return False
 
-def gptConfig(Position):
+
+def gptConfig(reportType, jobTitleReturnData):
     apt_key = loadUserFiles()
     if apt_key:
         api_key = apt_key[0]['api_key']
         openai.api_key = api_key
         openai.api_base = "https://api.chatanywhere.com.cn/v1"
-        ReturnGptData = gpt_35_api_stream([{'role': 'user', 'content': f'{Position}'}, ])
+        ReturnGptData = gpt_35_api_stream(
+            [
+                {
+                    'role': 'user',
+                    'content': f'我是一个正在实习的学生，我的岗位是{jobTitleReturnData}'
+                },
+                {
+                    'role': 'user',
+                    'content': f'帮我写一份{jobTitleReturnData}岗位实习{reportType}，内容大概 60 字左右，要求包含：1、实习项目；2、实习记录；3、实习总结。不要出现比如“XXX项目”、“A项目”之类的，你的回答只需要以 JSON 格式返回给我。'
+                }
+            ]
+        )
         time.sleep(int(random.uniform(15, 30)))
         return ReturnGptData
+
 
 if __name__ == '__main__':
     main.main()
